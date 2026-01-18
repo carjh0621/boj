c81ed4375014dcedbc0544101180f9af60490a53
carjh0621 <72341225+carjh0621@users.noreply.github.com>
2026-01-18 13:39:49 +0900

    boj 1967 자식 2개 고정 아님

--- 1967.cpp
+++ 1967.cpp
@@ -7,64 +7,61 @@
 using namespace std;
 
 int n;
-
-struct edge{
-    int dest;
-    int cost;
+int ans=0;
+struct node{
+    bool isleaf=false;
+    int left_child=0;
+    int right_child=0;
+    int left_edge_cost=0;
+    int right_edge_cost=0;
+    int diameter = 0;
+    int longest_path=0;
 };
 
-
-int search(int start_node, vector<vector<edge>> &e){
-    vector<int> dist(n+1,0);
-    vector<bool> visited(n+1, false);
-    priority_queue<pair<int,int>,vector<pair<int,int>>> q;
-    pair<int,int> dist_and_index;
-    dist[start_node]=0;
-    q.push(dist_and_index);
-
-    while(!q.empty()){
-        dist_and_index=q.top();
-        q.pop();
-        int cur=dist_and_index.second;
-        visited[cur]=true;
-        if(dist[cur]>dist_and_index.first) continue;
-        for(auto it=e[cur].begin();it!=e[cur].end();it++){
-            if(dist[it->dest] < dist[cur] + it->cost){
-                if(visited[it->dest]==false){
-                    dist[it->dest] = dist[cur] + it->cost;
-                    dist_and_index.first = dist[it->dest];
-                    dist_and_index.second = it->dest;
-                    q.push(dist_and_index);
-                }
-            }
-        }
+void search(int root, vector<node> &tree){
+    node& cur = tree[root];
+    if(cur.isleaf==false){
+        search(cur.left_child,tree);
+        search(cur.right_child,tree);
+        cur.longest_path = max(tree[cur.left_child].longest_path + cur.left_edge_cost, 
+                                tree[cur.right_child].longest_path + cur.right_edge_cost);
+        cur.diameter = tree[cur.left_child].longest_path + cur.left_edge_cost + 
+                        tree[cur.right_child].longest_path + cur.right_edge_cost;
+        if(ans<cur.diameter) ans=cur.diameter;
     }
-    sort(dist.begin(),dist.end());
-    return dist[0];
-
-
+    else{
+        return;
+    }
 }
+
+
 
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-
+ 
     cin>>n;
 
-    vector<vector<edge>> e(n+1,vector<edge> ());
+    vector<node> tree(n+1);
 
     for(int i=1;i<n;i++){
-        int s,d,c;
-        cin>>s>>d>>c;
-        e[s].push_back({d,c});
-        //e[d].push_back({s,c});
+        int p,c,w;
+        cin>>p>>c>>w;
+        if(tree[p].left_child==0) {
+            tree[p].left_child=c;
+            tree[p].left_edge_cost=w;
+        }
+        else{
+            tree[p].right_child=c;
+            tree[p].right_edge_cost=w;
+        }
     }
-    int ans=0;
-    for(auto it = e[1].begin(); it!=e[1].end();it++){
-        int tmp=0;
-        tmp = search(it->dest,e) + it->cost;
-        ans+=tmp;    
+    for(int i=0;i<=n;i++){
+        if(tree[i].left_child ==0 && tree[i].right_child==0) 
+            tree[i].isleaf=true;
     }
+    search(1,tree);
     cout<<ans<<"\n";
+    
 }
